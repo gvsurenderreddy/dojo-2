c9c17ee206e223d95b151140de5bd82044377bc5
Cristiane Iope Pelissari <cristiane.pelissari@objectos.com.br>
2013-07-05 17:38:32 -0300

    - Testes de Array De String

---
+++ objectos-dojo-team/src/test/java/br/com/objectos/dojo/cpelissari_estagiario/TesteArrayDeString.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2013 Objectos, Fábrica de Software LTDA.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package br.com.objectos.dojo.cpelissari_estagiario;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.util.List;
+
+import org.testng.annotations.Test;
+
+import com.google.common.collect.ImmutableList;
+
+/**
+ * @author cristiane.pelissari@objectos.com.br (Cristiane Iope Pelissari)
+ */
+@Test
+public class TesteArrayDeString {
+
+  private final ArrayDeString array = new ArrayDeStringImpl();
+
+  public void deve_transformar_String_em_ArrayDeString() {
+    String linha = "Fulano;05/07/2013;8000";
+
+    String[] _array = array.toArray(linha);
+    List<String> resp = ImmutableList.copyOf(_array);
+
+    assertThat(resp.size(), equalTo(3));
+    assertThat(resp.get(0), equalTo("Fulano"));
+    assertThat(resp.get(1), equalTo("05/07/2013"));
+    assertThat(resp.get(2), equalTo("8000"));
+  }
+
+}
