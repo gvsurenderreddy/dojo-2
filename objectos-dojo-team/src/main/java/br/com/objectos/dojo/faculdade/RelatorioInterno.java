8487c97856cd9ac9422ad14252da69035940dc2a
Carolene Bertoldi <carolene.bertoldi@objectos.com.br>
2013-10-08 16:08:29 -0300

    - iniciando segunda solução adotada

---
+++ objectos-dojo-team/src/main/java/br/com/objectos/dojo/faculdade/RelatorioInterno.java
@@ -0,0 +1,45 @@
+package br.com.objectos.dojo.faculdade;
+
+import java.util.List;
+
+import org.joda.time.DateTime;
+
+/*
+ * Copyright 2013 Objectos, Fábrica de Software LTDA.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+/**
+ * @author carolene.bertoldi@objectos.com.br (Carolene Reis Silva Bertoldi)
+ */
+public interface RelatorioInterno {
+
+  interface Construtor extends
+      br.com.objectos.comuns.base.Construtor<RelatorioInterno> {
+
+    Pedido getPedido();
+    DateTime getBaixa();
+
+    List<Object> getList();
+
+  }
+
+  int getId();
+
+  Pedido getPedido();
+  DateTime getBaixa();
+
+  List<Object> getList();
+
+}
